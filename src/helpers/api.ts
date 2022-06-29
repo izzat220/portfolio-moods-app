c92e3623bd5d2cfe29f60c697c6771b29006ad37
izzat alolwani <izzat.alolwani@gmail.com>
2022-06-29 10:51:39 +0400

    added server url

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: "http://localhost:8080",
+	baseURL: "https://portfolio-moods-server.herokuapp.com",
 	withCredentials: true,
 });
